098d45a5d4ea2befcc9be0e10cd6d30031314ef7
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2019-12-17 13:30:08 +0100

    mail send giveStar

--- src/main/java/ucll/project/ui/controller/GiveStar.java
+++ src/main/java/ucll/project/ui/controller/GiveStar.java
@@ -1,5 +1,6 @@
 package ucll.project.ui.controller;
 
+import extra.SimpleMail;
 import ucll.project.domain.star.Star;
 import ucll.project.domain.star.StarDB;
 import ucll.project.domain.user.UserService;
@@ -41,6 +42,12 @@ public class GiveStar extends RequestHandler {
         Star star = new Star(tags, description, sender_email, receiver_email);
         new StarDB().createStar(star);
 
+        String message = "Beste, "  + star.getUserReceiver().getFirstName() +"\nYou just received a star with tags:" + star.getTagsInString() + "\nWith description: " + star.getDescription() + "\nFrom" + star.getUserSender().getFirstName();
+        try {
+            SimpleMail.send(receiver_email,message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         response.sendRedirect("Controller");
     }
 }
