f4a82293affa2951aad3292a96d35321181bfe41
Robbe De Bie <robbe.debie@student.ucll.be>
2019-12-17 10:47:24 +0100

    usersHandler

--- src/main/java/ucll/project/ui/controller/Users.java
+++ src/main/java/ucll/project/ui/controller/Users.java
@@ -2,8 +2,10 @@ package ucll.project.ui.controller;
 
 import ucll.project.domain.user.UserService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class Users extends RequestHandler {
     public Users(String command, UserService userService){
@@ -11,8 +13,9 @@ public class Users extends RequestHandler {
     }
 
     @Override
-    public void handleRequest(HttpServletRequest request, HttpServletResponse response) {
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("users", this.getUserService().getUsers());
+        request.getRequestDispatcher("users.jsp").forward(request, response);
 
     }
 }
