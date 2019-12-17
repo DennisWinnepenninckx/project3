a1fce85766414ce27b3edd98a14afbf5dbc3edde
LouisVandePerre <louis.vandeperre@student.ucll.be>
2019-12-17 14:13:36 +0100

    Merge branch 'master' of C:\Users\louis\Documents\Toegepaste Informatica\2e Jaar\1e Semester\Projectweek\pw2-behaviour-driven-design with conflicts.

--- src/test/java/ucll/project/ui/UsersPageTest.java
+++ src/test/java/ucll/project/ui/UsersPageTest.java
@@ -7,12 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -32,5 +26,12 @@ public class UsersPageTest {
         driver.close();
     }
 
-
+    @Test
+    public void VisitUsersPageTest() {
+        driver.get(Config.BASE_URL);
+        WebElement link = driver.findElement(By.xpath("//a[text() = 'Users']"));
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        link.click();
+        assertEquals("All users", driver.getTitle());
+    }
 }
