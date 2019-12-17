0ccadeb131626f904da7359d37408a55d844cfd7
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2019-12-17 12:00:22 +0100

    star has message now

--- src/main/java/ucll/project/ui/controller/mail.java
+++ src/main/java/ucll/project/ui/controller/mail.java
@@ -1,6 +1,7 @@
 package ucll.project.ui.controller;
 
 import extra.SimpleMail;
+import ucll.project.domain.star.Star;
 import ucll.project.domain.user.UserService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,16 +9,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class mail extends RequestHandler {
-    public mail(String command, UserService userService) {
+    private Star star;
+    public mail(String command, UserService userService, Star star) {
         super(command, userService);
+        this.star = star;
     }
 
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String receiver = request.getParameter("receiver");
-        try{
-            SimpleMail.test(receiver);
-        }catch (Exception e){
+        try {
+            String message = "Dear " + star.getUserReceiver().getFirstName() +
+                    ", \n You just received a star with the following tags: " +
+                    star.getTagsInString() + "\n from " + star.getUserSender().getEmail() +
+                    "\nDescription:\n" + "\nGood job!";
+            SimpleMail.send(star,message);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
