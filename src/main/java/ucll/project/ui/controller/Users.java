ddc2a1d94a77e42708f9099e6938f8bf75b17338
KarelSavonet <43341133+KarelSavonet@users.noreply.github.com>
2019-12-18 15:50:21 +0100

    not every user can see the users through url

--- src/main/java/ucll/project/ui/controller/Users.java
+++ src/main/java/ucll/project/ui/controller/Users.java
@@ -1,11 +1,13 @@
 package ucll.project.ui.controller;
 
 import ucll.project.db.DBController;
+import ucll.project.domain.user.User;
 import ucll.project.domain.user.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Users extends RequestHandler {
@@ -16,7 +18,17 @@ public class Users extends RequestHandler {
 
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("users", this.getUserService().getAllUsers());
-        request.getRequestDispatcher("users.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        User sessionUser = (User) session.getAttribute("user");
+        if (sessionUser == null || !sessionUser.isAdmin()){
+            request.setAttribute("notAuthorized","You don't have the rights to perform this action.");
+            HandlerFactory h = new HandlerFactory();
+            RequestHandler rh = h.getHandler("Index",this.getUserService());
+            rh.handleRequest(request,response);
+        }
+        else{
+            request.setAttribute("users", this.getUserService().getAllUsers());
+            request.getRequestDispatcher("users.jsp").forward(request, response);
+        }
     }
 }
