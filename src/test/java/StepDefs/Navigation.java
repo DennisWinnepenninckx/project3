e17feb87871114d2931fa5bea920256696e28c30
Bram <bram.verbist@student.ucll.be>
2019-12-18 10:25:23 +0100

    new featurefiles

---
+++ src/test/java/StepDefs/Navigation.java
@@ -0,0 +1,35 @@
+package StepDefs;
+
+import io.cucumber.java.en.*;
+
+public class Navigation {
+    @Given("Karel is on a page")
+    public void karel_is_on_a_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println();
+    }
+
+    @When("Karel hovers over the navigation")
+    public void karel_hovers_over_the_navigation() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println();
+    }
+
+    @Then("the different navigation bars are highlighted when the mouse is above them")
+    public void the_different_navigation_bars_are_highlighted_when_the_mouse_is_above_them() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println();
+    }
+
+    @When("Karel looks at the navigation bar")
+    public void karel_looks_at_the_navigation_bar() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println();
+    }
+
+    @Then("The current page has another color")
+    public void the_current_page_has_another_color() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println();
+    }
+}
