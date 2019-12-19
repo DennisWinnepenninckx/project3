b0abd75716b1e8ecba3cf7152cde65df6900504f
LouisVandePerre <louis.vandeperre@student.ucll.be>
2019-12-19 14:52:20 +0100

    Mooie font

--- src/main/java/ucll/project/application/main.java
+++ src/main/java/ucll/project/application/main.java
@@ -7,19 +7,19 @@ import ucll.project.domain.user.User;
 public class main {
     public static void main(String[] args) {
         //ExcelReadWrite.read();
-/*
+
         User user = new User();
-        user.setEmail("andreas.milants@gmail.com");
-        user.setNewPassword("pushbanaan");
-        user.setLastname("Milants");
-        user.setFirstName("Andreas");
-        user.setSuperUser(true);
+        user.setEmail("superuser@test.be");
+        user.setNewPassword("superuserpaswoord");
+        user.setLastname("TEST");
+        user.setFirstName("SUPERUSER");
+        user.setSuperUser(false);
         user.setAdmin(true);
         user.setManager(false);
 
         DBController.getInstance().createUser(user);
 
- */
+
         System.out.println(DBController.getInstance().getAllStars());
 
         //db.createUser(user);
