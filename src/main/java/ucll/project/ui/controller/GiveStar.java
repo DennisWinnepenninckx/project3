71ea3cfa93bf0dab0d506f89cac94f35e65eec7f
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2019-12-19 10:40:55 +0100

    mail css/html

--- src/main/java/ucll/project/ui/controller/GiveStar.java
+++ src/main/java/ucll/project/ui/controller/GiveStar.java
@@ -72,8 +72,136 @@ public class GiveStar extends RequestHandler {
 
                 Star star = new Star(tagStrings, description, sender_email, receiver_email);
                 getUserService().createStar(star);
-                String message = "Beste, " + star.getUserReceiver().getFirstName() + "\nYou just received a star with tags:" + star.getTagsInString() + "\nWith description: " + star.getDescription() + "\nFrom" + star.getUserSender().getFirstName();
-                try {
+                String message = "<!doctype html>\n" +
+                        "<html>\n" +
+                        "<header>\n" +
+                        "    <style>\n" +
+                        "        html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, font, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, caption {\n" +
+                        "            margin: 0;\n" +
+                        "            padding: 0;\n" +
+                        "            border: 0;\n" +
+                        "            outline: 0;\n" +
+                        "            font-size: 100%;\n" +
+                        "            vertical-align: baseline;\n" +
+                        "            background: transparent;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        h1 {\n" +
+                        "            font-size: 2.5em;\n" +
+                        "            background-color: #144a91;\n" +
+                        "            color: white;\n" +
+                        "            text-align: center;\n" +
+                        "            padding: 10px 0;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        body {\n" +
+                        "        / / background-color: #144a91;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "\n" +
+                        "        .star-class {\n" +
+                        "            padding: 2rem;\n" +
+                        "            box-shadow: 0 0 5px #AAAAAA;\n" +
+                        "            display: flex;\n" +
+                        "            flex-direction: row;\n" +
+                        "            align-items: center;\n" +
+                        "            background: #DDEEFF;\n" +
+                        "            border-radius: 10px;\n" +
+                        "            font-family: 'Libre Franklin', sans-serif;\n" +
+                        "            font-size: 1.2rem;\n" +
+                        "            color: #144a91;\n" +
+                        "            width: 50%;\n" +
+                        "            margin: auto;\n" +
+                        "            margin-top: 1em;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class .description {\n" +
+                        "            font-family: 'Permanent Marker', cursive;\n" +
+                        "            font-size: 2rem;\n" +
+                        "        }\n" +
+                        "        .description{\n" +
+                        "            padding-top: 15px;\n" +
+                        "            padding-bottom: 15px;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class > div:first-child {\n" +
+                        "            display: flex;\n" +
+                        "            flex-direction: column;\n" +
+                        "            align-items: center;\n" +
+                        "            text-align: center;\n" +
+                        "            width: 10%;\n" +
+                        "            margin: 5%;\n" +
+                        "            line-height: 1.6rem;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class > div:nth-child(2) {\n" +
+                        "            max-width: 75%;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class img {\n" +
+                        "            margin-bottom: .5rem;\n" +
+                        "            border-radius: 50%;\n" +
+                        "            border: 1px solid #144a91;\n" +
+                        "            width: 100%;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class ul {\n" +
+                        "            display: flex;\n" +
+                        "            list-style: none;\n" +
+                        "            padding: 0;\n" +
+                        "            margin: 0;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class .sender {\n" +
+                        "            font-weight: bold;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class ul li {\n" +
+                        "            display: flex;\n" +
+                        "            align-items: center;\n" +
+                        "            padding: .4rem;\n" +
+                        "            background: #3595f6;\n" +
+                        "            border-radius: 3px;\n" +
+                        "            margin: .3rem;\n" +
+                        "            color: white;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class .material-icons {\n" +
+                        "            margin: 0;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class b {\n" +
+                        "            margin-right: .2rem;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "        .star-class th {\n" +
+                        "            padding-right: 2rem;\n" +
+                        "        }\n" +
+                        "\n" +
+                        "    </style>\n" +
+                        "</header>\n" +
+                        "<body>\n" +
+                        "<head>\n" +
+                        "    <h1>Stars4everyone</h1>\n" +
+                        "</head>\n" +
+                        "<article>\n" +
+                        "    <div class=\"star-class\">\n" +
+                        "        <div>\n" +
+                        "            <img src=\"https://behaviour-driven-design.projectweek.be/static/imgs/star-small.png\" width=100%>\n" +
+                        "            <span class=\"receiver\"><b>"+ star.getUserReceiver().getFirstName()+ " " + star.getUserReceiver().getLastname()+"</b></span>\n" +
+                        "        </div>\n" +
+                        "        <div>\n" +
+                        "            <span class=\"sender\">You got a star from <b>"+ star.getUserSender().getFirstName()+ " " + star.getUserSender().getLastname() +"</b>! </span>\n" +
+                        "            <p class=\"description\">"+ star.getDescription()+"</p>\n" +
+                        "            <ul class=\"tag-list\">\n" +
+                        //getTagHTML()
+                        //+
+                        "            </ul>\n" +
+                        "        </div>\n" +
+                        "    </div>\n" +
+                        "</article>\n" +
+                        "</body>\n" +
+                        "</html>";                try {
                     SimpleMail.send(receiver_email, message);
                 } catch (Exception e) {
                     e.printStackTrace();
